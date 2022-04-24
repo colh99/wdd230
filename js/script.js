f546502b0b5becb00379593549eedfa94f57e3b5
colh99 <colbyhale199@gmail.com>
2022-04-23 22:05:07 -0600

    Added finishing touches

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+// Sets the last modified to the current time and date
 document.querySelector("#lastmod").textContent = document.lastModified;
+
+// Sets the copyright year to the current year
+const todaysdate = new Date();
+document.querySelector("#copyrightyear").textContent = todaysdate.getFullYear();
