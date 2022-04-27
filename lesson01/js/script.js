db6034ea8c726921b63c36c6d0048dfd2bb0b9fb
colh99 <colbyhale199@gmail.com>
2022-04-26 22:58:58 -0600

    Set link to Lesson 02 page from index.html, and fixed a lesson 1 script.js references that got accidentally deleted.

--- lesson01/js/script.js
+++ lesson01/js/script.js
@@ -1,6 +1,6 @@
 // Sets the copyright year to the current year
 const todaysdate = new Date();
-document.querySelector("#").textContent = todaysdate.getFullYear();
+document.querySelector("#copyrightyear").textContent = todaysdate.getFullYear();
 
 // Sets the last modified to the current time and date
-document.querySelector("#").textContent = document.lastModified;
+document.querySelector("#lastmod").textContent = document.lastModified;
