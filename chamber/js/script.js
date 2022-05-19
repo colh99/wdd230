9eca7f257a87218d892658173dde05e7d2ef8b3d
colh99 <colbyhale199@gmail.com>
2022-05-19 14:51:15 -0600

    Completed footer

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,8 +1,15 @@
+// Hamburger Menu
 function toggleMenu() {
-    console.log("It worked!")
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+// Sets the copyright year to the current year
+const todaysdate = new Date();
+document.querySelector("#copyrightyear").textContent = todaysdate.getFullYear();
+
+// Sets the last modified to the current time and date
+document.querySelector("#lastmod").textContent = document.lastModified;
