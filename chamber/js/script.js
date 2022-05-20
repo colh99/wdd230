0e7359cf7592b080e50aa6c2662831b251189208
colh99 <colbyhale199@gmail.com>
2022-05-19 20:40:11 -0600

    Completed home page layout wireframe

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -7,9 +7,21 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
-// Sets the copyright year to the current year
+// Get a date object to use
 const todaysdate = new Date();
+
+// Sets today's date at the top of the page
+const datefieldUK = document.querySelector("#todays-date"); // for european/family history format with day first.
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(todaysdate);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+// Sets the copyright year to the current year
 document.querySelector("#copyrightyear").textContent = todaysdate.getFullYear();
 
 // Sets the last modified to the current time and date
 document.querySelector("#lastmod").textContent = document.lastModified;
+
