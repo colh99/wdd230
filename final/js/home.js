24048c30a61b24d928f519f6822e5bbaa4914485
colh99 <colbyhale199@gmail.com>
2022-07-18 23:44:40 -0600

    Services pages

--- final/js/home.js
+++ final/js/home.js
@@ -44,3 +44,30 @@ function displayTemples(temple) {
     templeCard.appendChild(dedicated);
     templeCard.appendChild(linkToTemplesP);
 }
+
+// Weather
+// Select HTML elements to edit
+const temperature = document.getElementById('temperature');
+const weatherCondition = document.getElementById('weather-condition');
+
+// Assemble url 
+const base_url = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+const lat = "35.591871115293955";
+const lon= "-97.72664714034867";
+const appid = "f2de38b59fa3ab7886f1e42ef1440d98";
+const units = "imperial";
+
+const url = `${base_url}${lat}&lon=${lon}&exclude=hourly&appid=${appid}&units=${units}`;
+
+// Fetch!
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    
+    // Temperature
+    const temperatureData = data["current"]["temp"];
+    temperature.textContent = Math.round(temperatureData);
+    
+    // Condition
+    weatherCondition.textContent = data["current"]["weather"]["main"];
+});
